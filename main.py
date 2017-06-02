f15dc257923400dbc6fc0d1989bf0dd3851d6cf5
Ariel Hernández Ramírez <arielhr1987@gmail.com>
2017-06-01 23:55:39 -0700

    mds ans tsne experiments

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
-import problems
+import json
+import os
 
-problems.onemax.main()
+path = 'data'
+
+
+def main():
+    # problems.onemax.main()
+    update_info()
+
+
+def update_info():
+    json_files = [pos_json for pos_json in os.listdir(path) if (pos_json.endswith('.json') and
+                                                                not pos_json.startswith('info'))]
+    info_data = list()
+    for filename in json_files:
+        with open(os.path.join(path, filename)) as json_file:
+            # do something with your json; I'll just print
+            json_data = json.load(json_file)
+            new_data = json_data['extra']
+            new_data['filename'] = filename
+            info_data.append(new_data)
+
+    with open(os.path.join(path, 'info.json'), 'w') as outfile:
+        json.dump(info_data, outfile)
+    print 'File info.json updated'
+    return
+
+
+if __name__ == "__main__":
+    main()
--- problems/common.py
+++ problems/common.py
@@ -29,9 +29,8 @@ def enable_print():
 def save(all, extra={}):
     for method in methods:
         extra['method'] = method
-        # save_data(all, extra)
+        save_data(all, extra)
 
-    update_info()
     return
 
 
@@ -94,20 +93,3 @@ def save_data(all, extra):
     with open(os.path.join(path, filename), 'w') as file:
         json.dump(data, file)
     print 'Saved to %s' % os.path.join(path, filename)
-
-
-def update_info():
-    json_files = [pos_json for pos_json in os.listdir(path) if (pos_json.endswith('.json') and
-                                                                not pos_json.startswith('info'))]
-    info_data = list()
-    for filename in json_files:
-        with open(os.path.join(path, filename)) as json_file:
-            # do something with your json; I'll just print
-            json_data = json.load(json_file)
-            new_data = json_data['extra']
-            new_data['filename'] = filename
-            info_data.append(new_data)
-
-    with open(os.path.join(path, 'info.json'), 'w') as outfile:
-        json.dump(info_data, outfile)
-    return
